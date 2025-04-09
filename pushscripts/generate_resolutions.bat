66f039c5fec4241eeca16eb54aa2d1ca9b202953
NJ <55053061+DoritoMojito@users.noreply.github.com>
2025-04-09 15:23:41 +1000

    Automated update

--- pushscripts/generate_resolutions.bat
+++ pushscripts/generate_resolutions.bat
@@ -18,14 +18,14 @@ if errorlevel 1 (
 :: Create output directory
 if not exist "%output_dir%" mkdir "%output_dir%"
 
+echo Processing Images
 :: Process images
 for %%f in ("%source_dir%\*.png", "%source_dir%\*.jpg", "%source_dir%\*.jpeg", "%source_dir%\*.webp") do (
     call :process_image "%%f" "%%~nf" "%%~xf"
 )
 
-echo.
 echo Image processing complete!
-pause
+::pause
 exit /b
 
 :process_image
@@ -33,21 +33,24 @@ set "input=%~1"
 set "name=%~2"
 set "ext=%~3"
 
-echo Processing: %name%%ext%
+echo    Processing: %name%%ext%
 
 :: Create version directory
 mkdir "%output_dir%\%name%" >nul 2>&1
 
 :: Create small (500px)
-echo Creating %name%-small%ext%
+::echo Creating %name%-small%ext%
 magick "%input%" -resize 500x -quality 85 -strip "%output_dir%\%name%\%name%-small%ext%"
 
 :: Create medium (800px)
-echo Creating %name%-medium%ext%
+::echo Creating %name%-medium%ext%
 magick "%input%" -resize 800x -quality 85 -strip "%output_dir%\%name%\%name%-medium%ext%"
 
 :: Create large (1200px)
-echo Creating %name%-large%ext%
+::echo Creating %name%-large%ext%
 magick "%input%" -resize 1200x -quality 85 -strip "%output_dir%\%name%\%name%-large%ext%"
+
+echo    Finished processing: %name%%ext%
+echo.
 
 exit /b
